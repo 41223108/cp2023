8b93fe30e2796b1170828dfa1de1dd0f8fdedb09
41223108 <144768295+41223108@users.noreply.github.com>
2023-12-31 08:49:52 +0000

    新增ANSIC作業內容

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 楊怡婷\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : August 22, 2005\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0908-315193\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
